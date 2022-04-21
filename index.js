13dbc96417bd138fb53db6a576a0fa4a12ee2ba0
zhe <42285226@qq.com>
2022-04-22 00:30:38 +0800

    '2'

--- index.js
+++ index.js
@@ -2,38 +2,38 @@ const express = require('express')
 const mongoose = require("mongoose")
 
 const app = express();
-const PORT = 4000;
+const PORT = 8080;
 
-var DB = 'mongodb://root:password@127.0.0.1:27017/admin'
+/*var DB = 'mongodb://root:password@127.0.0.1:27017/admin'
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
   })
   .then(() => console.log('DB connection successful!'))
   .catch(err => console.log(err));
-  
+  */
 // create a schema
-const studentSchema = new mongoose.Schema({
+/*const studentSchema = new mongoose.Schema({
     name: String,
     subjects: [String]
 });
-
+*/
 // create a model with studentSchema
-const Student = mongoose.model('Student', studentSchema);
+// const Student = mongoose.model('Student', studentSchema);
 
 
 // Create a new document
-const stud = new Student({
+/*const stud = new Student({
     name: 'Madison Hyde',
     subjects: ['DBMS', 'OS', 'Graph Theory', 'Internet Programming']
 });
 
-
+*/
 
 app.listen(PORT, ()=>{
-	console.log('run on ');
+	console.log('run on '+PORT);
 })
 app.get("/", async (req,res)=>{
-	await stud.save();
+	// await stud.save();
 	res.send('One entry added')
 })
